72d262d07f0e6d53eadd6024a0baa6e08489dae8
khanh Vo <vokar@sheridancollege.ca>
2025-01-27 23:51:02 -0500

    Not running the animation by itself?

--- Assets/Scripts/Blinking Stars.cs
+++ Assets/Scripts/Blinking Stars.cs
@@ -11,6 +11,8 @@ public class BlinkingStars : MonoBehaviour
     [Range (0,2)]// Range of the min and max it will go,applied to the variable below (t), kinda like constraint but not yet
     public float t; //just a variable represent for the height/ time in this, to use in the code
 
+    Vector2 two = new Vector2(2, 2); // A Vector2 with x = 2, y = 2
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,14 +25,15 @@ public class BlinkingStars : MonoBehaviour
     {
         //update the time value +1 everytime
         t += Time.deltaTime;
-        //reset if t>1
+
+        //reset if t>2
         if (t > 2)
         {
             t = 0;
         }
         //we do animation on the object so use transform obviously
         //vector2 since we only use x,y value. 
-        transform.localScale = Vector2.one * curve.Evaluate(t); 
+        transform.localScale = two * curve.Evaluate(t); 
        
     }
 }
