2f592db7f5ab39470146171adb62bb78f388b2a4
khanh Vo <vokar@sheridancollege.ca>
2025-01-30 19:06:05 -0500

    mouse follow, but applied mouse click condition, Not woking, but progress

---
+++ Assets/Scripts/CatEyes_Movement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatEyes_Movement : MonoBehaviour
+
+//Combine the lerp for the contraint position and the mouse follow!
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/LazeMouse.cs
+++ Assets/Scripts/LazeMouse.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//Click toshow then click again to off
+// combine the mouse follow and mouse button video
 public class LazeMouse : MonoBehaviour
 {
+    public Vector3 mousepos;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,16 @@ public class LazeMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+  
+        transform.position = mousepos;
+        if (Input.GetMouseButtonDown(0))//0 to left click
+            //if mouse pressed true z= -10 
+        {
+            mousepos.z = -10;
+        }// else z= 0 
+        mousepos.z = 0;
     }
+
+
 }
