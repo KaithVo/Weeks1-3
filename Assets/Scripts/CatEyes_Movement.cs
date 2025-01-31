d32f0d40e5ddb7a04b63db12fbe0831b99d3d62b
khanh Vo <vokar@sheridancollege.ca>
2025-01-30 19:45:14 -0500

    making cat eye move with the lazer, so put cat eyes under as the child of the lazer.
    Get conflicted because of the speed/ frame

--- Assets/Scripts/CatEyes_Movement.cs
+++ Assets/Scripts/CatEyes_Movement.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CatEyes_Movement : MonoBehaviour
 
 //Combine the lerp for the contraint position and the mouse follow!
 {
+
+    public Vector2 start = new Vector2(2, 0);
+    public Vector2 end = new Vector2(-2, 1);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,6 @@ public class CatEyes_Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.Lerp(start, end, 0.2f);
     }
 }
